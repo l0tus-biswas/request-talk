0ea7761eb793bd094c8e0fc5fb85dd364503c30c
Lotus Biswas <102558895+dev-lotus@users.noreply.github.com>
2022-05-05 10:29:34 +0530

    Deploy

--- CalendlyApp/src/app/services/event-service/event.service.ts
+++ CalendlyApp/src/app/services/event-service/event.service.ts
@@ -13,19 +13,19 @@ export class EventService {
 
 
   getAllAvaibility(userId: number, usertoken: string): Observable<IAvailibility[]> {
-    return this.http.get<IAvailibility[]>("https://localhost:44305/api/Calendly/GetAllAvaibility?userId=" + userId + "&usertoken=" + usertoken).pipe(catchError(this.errorHandler));
+    return this.http.get<IAvailibility[]>("https://appointly.azurewebsites.net/api/Calendly/GetAllAvaibility?userId=" + userId + "&usertoken=" + usertoken).pipe(catchError(this.errorHandler));
   }
 
   getAllEvents(userId: number, usertoken: string): Observable<IEvents[]> {
-    return this.http.get<IEvents[]>("https://localhost:44305/api/Calendly/GetAllEvents?userId=" + userId + "&usertoken=" + usertoken).pipe(catchError(this.errorHandler));
+    return this.http.get<IEvents[]>("https://appointly.azurewebsites.net/api/Calendly/GetAllEvents?userId=" + userId + "&usertoken=" + usertoken).pipe(catchError(this.errorHandler));
   }
 
   getEventById(userId: number, usertoken: string, eventId: number): Observable<IEvents[]> {
-    return this.http.get<IEvents[]>("https://localhost:44305/api/Calendly/GetEventById?userId=" + userId + "&usertoken=" + usertoken + "&eventId=" + eventId).pipe(catchError(this.errorHandler));
+    return this.http.get<IEvents[]>("https://appointly.azurewebsites.net/api/Calendly/GetEventById?userId=" + userId + "&usertoken=" + usertoken + "&eventId=" + eventId).pipe(catchError(this.errorHandler));
   }
 
   addNewEvent(userId: number, userToken: string, title: string, url: string, length: number, availabilityId: number, description: string, location: string): Observable<boolean> {
-    return this.http.post<boolean>("https://localhost:44305/api/Calendly/AddNewEvent", {
+    return this.http.post<boolean>("https://appointly.azurewebsites.net/api/Calendly/AddNewEvent", {
       userId: userId,
       userToken: userToken,
       title: title,
@@ -41,7 +41,7 @@ export class EventService {
     url: string, length: number, availabilityId: number, eventName: string, optInBooking: string, disableGuests: string, hideEventType: string, timeSlotIntervals: number): Observable<boolean> {
     console.log(eventId, userId, userToken, title, description, location, url, length, availabilityId, eventName, optInBooking, disableGuests,hideEventType, timeSlotIntervals);
    
-    return this.http.put<boolean>("https://localhost:44305/api/Calendly/UpdateEventDetails", {
+    return this.http.put<boolean>("https://appointly.azurewebsites.net/api/Calendly/UpdateEventDetails", {
       eventId: eventId,
       userId: userId,
       userToken: userToken,
@@ -62,7 +62,7 @@ export class EventService {
 
 
   deleteEvent(userId: number, usertoken: string, eventId: number): Observable<boolean> {
-    return this.http.delete<boolean>("https://localhost:44305/api/Calendly/DeleteEventById?userId=" + userId + "&usertoken=" + usertoken + "&eventId=" + eventId).pipe(catchError(this.errorHandler))
+    return this.http.delete<boolean>("https://appointly.azurewebsites.net/api/Calendly/DeleteEventById?userId=" + userId + "&usertoken=" + usertoken + "&eventId=" + eventId).pipe(catchError(this.errorHandler))
   }
   errorHandler(error: HttpErrorResponse) {
     console.error(error);
