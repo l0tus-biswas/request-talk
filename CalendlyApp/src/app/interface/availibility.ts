e3f959ce3d69c40f7db631fb363578045f62ebba
Lotus Biswas <131962991+l0tus-biswas@users.noreply.github.com>
2023-05-02 14:01:38 +0530

    Delete CalendlyApp directory

--- CalendlyApp/src/app/interface/availibility.ts
+++
@@ -1,8 +0,0 @@
-export interface IAvailibility{
-    availabilityId: number,
-    availabilityName: string,
-    userId: number,
-    userToken: string,
-    weeksAvailability: string,
-    timezone: string
-}
